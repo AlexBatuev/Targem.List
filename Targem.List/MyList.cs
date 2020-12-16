fcc30ba26e95bf18313b74463f30a0d6ab13eed0
Alexander Batuev <batuev.prizrak@gmail.com>
2020-12-16 10:44:40 +0900

    Добавлен класс MyList<T>, реализующий интерфейс IList<T>

---
+++ Targem.List/MyList.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Targem.List
+{
+    class MyList<T> : IList<T>
+    {
+        private T[] _items = new T[8];
+
+        public MyList()
+        {
+            Count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (Count == _items.Length)
+            {
+                ResizeItems();
+            }
+
+            _items[Count++] = item;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            }
+
+            if (Count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("Invalid target array length", nameof(array));
+            }
+
+            for (var i = 0; i < Count; i++)
+            {
+                array.SetValue(_items[i], arrayIndex++);
+            }
+        }
+
+        public int Count { get; private set; }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.Take(Count).GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for (var i = 0; i < _items.Length; i++)
+            {
+                if (item != null && item.Equals(_items[i]))
+                {
+                    return i;
+                }
+
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            if (Count == _items.Length)
+                ResizeItems();
+                    
+            for (var i = Count - 1; i > index; i--)
+            {
+                _items[i] = _items[i - 1];
+            }
+
+            _items[index] = item;
+            Count++;
+        }
+
+        public bool IsReadOnly => false;
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            for (var i = index; i < Count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            Count--;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index > Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                _items[index] = value;
+            }
+        }
+
+        private void ResizeItems()
+        {
+            Array.Resize(ref _items, _items.Length * 2); // todo
+        }
+    }
+}
---
+++ Targem.List/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Targem.List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MyList<string>();
+
+            // Add
+            Console.WriteLine("Populate MyList");
+            test.Add("one");
+            test.Add("two");
+            test.Add("three");
+            test.Add("four");
+            test.Add("five");
+            test.Add("six");
+            test.Add("seven");
+            test.Add("eight");
+            test.Add("nine");
+            foreach (var t in test)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine();
+
+            // Count
+            Console.WriteLine("Elements count");
+            Console.WriteLine($"Index of \"seven\":  {test.Count}");
+            Console.WriteLine();
+
+            // Remove
+            Console.WriteLine("Remove elements from the list");
+            test.Remove("six");
+            test.RemoveAt(8);
+            foreach (var t in test)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine();
+
+            // Insert
+            Console.WriteLine("Insert an element into the list");
+            test.Insert(4, "number");
+            foreach (var t in test)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine();
+
+            // Contains
+            Console.WriteLine("Check for specific elements in the list");
+            Console.WriteLine($"List contains \"three\": {test.Contains("three")}");
+            Console.WriteLine($"List contains \"ten\": {test.Contains("ten")}");
+            Console.WriteLine();
+
+            // Index
+            Console.WriteLine("Index of element");
+            Console.WriteLine($"Index of \"seven\": {test.IndexOf("seven")}");
+            Console.WriteLine();
+
+            // Iterator
+            Console.WriteLine("Get elements of the list by index");
+            Console.WriteLine($"Get [1]: {test[1]}");
+            Console.WriteLine($"Get [3]: {test[3]}");
+            Console.WriteLine($"Get [5]: {test[5]}");
+            Console.WriteLine();
+
+            // CopyTo
+            Console.WriteLine("Copy to array");
+            var array = new string[10];
+            test.CopyTo(array, 2);
+            foreach (var a in array)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine();
+
+            //
+            Console.ReadKey();
+        }
+    }
+}
